c6d595e9ca5138879813165b41202532832f9ee8
Manelle Nouar <60315534+manellenr@users.noreply.github.com>
2020-02-23 20:35:33 +0100

    Update Bistromatique.c

--- Bistromatique.c
+++ Bistromatique.c
@@ -5,55 +5,164 @@
 ** Started on Thu Jan 24 20:21:33 2020 Thibaut Cornolti
 */
 
-static char	* get_result(char * temps1, char * temps2, int * negative)
+#include "../include/my_struct.h"
+
+int	priority(t_list *list, char c)
 {
-  if (temps1[0] == '-' && temps2[0] == '-')
-    {
-      *negative = 1;
-      return (my_infin_add(&temps1[1], &temps2[2]));
-    }
-  else if (temps1[0] == '-' && temps2[0] != '-')
-    if (my_infin_cmp(&temps1[1], temps2) > 0)
-      {
-	*negative = 1;
-	return (my_infin_sub(&temps1[1], temps2));
-      }
-    else
-      return (my_infin_sub(temps2, &temps1[1]));
-  else if (temps1[0] != '-' && temps2[0] == '-')
-      if (my_infin_cmp(temps1, &temps2[1]) < 0)
-	{
-	  *negative = 1;
-	  return (my_infin_sub(&temps2[1], temps1));
-	}
-      else
-	return (my_infin_sub(temps1, &temps2[1]));
+  int	t;
+
+  t = is_in_str(c, list->ops);
+  if (t > 4)
+    return (3 * (list->level) + 1);
   else
-    return (my_infin_add(temps1, temps2));
+    return (3 * (list->level));
 }
 
-char * add(char *nb1, char *nb2)
+int		ope_only(t_list *list, char *str)
 {
-  char * res ;
-  char * temps1 ;
-  char * temps2 ;
-  int negative;
+  int		t;
+  int		u;
+  int		n;
+  t_token	*token;
 
-  negative = 0;
-  temps1 = my_strdup(nb1);
-  temps2 = my_strdup(nb2);
-  if (!are_correct_operandes(temps1, temps2))
-    return (NULL);
-  res = remove_zeros(get_result(temps1, temps2, &negative));
-  free(temps1);
-  temps1 = 0;
-  free(temps2);
-  temps2 = 0;
-  if (negative)
+  t = is_in_str(str[0], list->ops);
+  u = is_in_str(str[1], list->ops);
+  n = 1;
+  if (t == 1 || t == 2)
+    ope_function(list, str, t, u);
+  else
+    ope_funct_other(list, t, u);
+  if (u == 3 || u == 4)
     {
-      my_revstr(res);
-      res = (my_strcat(res, my_strdup("-")));
-      my_revstr(res);
+      if (t == 2)
+	{
+	  token = tokenise(NULL, 3, priority(list, str[0]), 0);
+	  add_to_list(list, token);
+	}
+      n = n + unary(list, str + 1);
     }
-  return (res);
+  return (n);
+}
+
+void		unary_parenthesis(t_list *list, int s)
+{
+  char		*token_str;
+  t_token	*token;
+  t_token	*mult;
+
+  if (s == -1)
+    {
+      token_str = malloc(2);
+      if (token_str == NULL)
+	my_error(ERROR_MSG);
+      token_str[1] = 0;
+      token_str[0] = 2;
+      token = tokenise(token_str, 0, 0, -1);
+      add_to_list(list, token);
+      mult = tokenise(NULL, 5, priority(list, list->ops[4]) + 1, 0);
+      add_to_list(list, mult);
+    }
+}
+
+int	unary(t_list *list, char *str)
+{
+  int	i;
+  int	n;
+  char	s;
+
+  i = 0;
+  n = 0;
+  s = 1;
+  while (str[i] == list->ops[2] || str[i] == list->ops[3])
+    {
+      if (str[i] == list->ops[3])
+	s = -s;
+      i = i + 1;
+    }
+  if (str[i] == list->ops[0])
+    unary_parenthesis(list, s);
+  else if (is_in_str(str[i], list->base))
+    n = add_nbr(list, str + i, s);
+  else
+    {
+      free_list(list);
+      my_error(SYNTAXE_ERROR_MSG);
+    }
+  n = n + i;
+  return (n);
+}
+int		bistro(char **av)
+{
+  char		*expr;
+  t_list	*parse;
+  t_token	*result;
+
+  expr = rd_expr(av[3]);
+  parse = lexer(expr, av[1], av[2]);
+  parser(parse);
+  result = calc(parse);
+  base_to_str(av[1], result->data);
+  if (result->sign == -1)
+    my_putchar(av[2][3]);
+  my_putstr(result->data);
+  free_token(result);
+  free(parse);
+  return (0);
+}
+
+void	add_stack(t_token **stack, t_token *token)
+{
+  token->next = *stack;
+  *stack = token;
+}
+
+void		prioprs(t_token **stack, t_token *token, t_list *st_list)
+{
+  t_token	*tmp2;
+
+  if ((*stack) == NULL)
+    {
+      add_stack(stack, token);
+    }
+  else if (token->priority > (*stack)->priority)
+    {
+      add_stack(stack, token);
+    }
+  else
+    {
+      while (*stack != NULL && token->priority <= (*stack)->priority)
+	{
+	  tmp2 = (*stack)->next;
+	  add_to_list(st_list, *stack);
+	  *stack = tmp2;
+	}
+      add_stack(stack, token);
+    }
+}
+
+void		parser(t_list *list)
+{
+  t_token	*stack;
+  t_token	*tmp;
+  t_token	*token;
+
+  stack = NULL;
+  token = list->first;
+  list->first = token;
+  list->last = token;
+  while (token != NULL)
+    {
+      tmp = token->next;
+      if (token->type == 0)
+	add_to_list(list, token);
+      else
+	prioprs(&stack, token, list);
+      token = tmp;
+    }
+  while (stack != NULL)
+    {
+      tmp = stack->next;
+      add_to_list(list, stack);
+      stack = tmp;
+    }
 }
